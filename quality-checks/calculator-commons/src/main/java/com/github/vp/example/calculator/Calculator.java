d94916c07f326eedaf778ec9a74c11b947e7b58d
Vimal Parakhiya <vimalpar@INvimalpar.local>
2017-05-07 14:42:06 +0530

    Sample multi-module projects with pmd, checkstyle and findbugs configured

---
+++ quality-checks/calculator-commons/src/main/java/com/github/vp/example/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.github.vp.example.calculator;
+
+/**
+ * Created by vimalpar on 07/05/17.
+ */
+public interface Calculator {
+    double add(double a, double b);
+    double substract(double a, double b);
+    double multiply(double a, double b);
+    double divide(double a, double b);
+}
---
+++ quality-checks/calculator-impl/simple-calculator/src/main/java/com/github/vp/example/calculator/impl/SimpleCalculator.java
@@ -0,0 +1,25 @@
+package com.github.vp.example.calculator.impl;
+
+import com.github.vp.example.calculator.Calculator;
+
+/**
+ * Created by vimalpar on 07/05/17.
+ */
+public class SimpleCalculator implements Calculator {
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double substract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+}
