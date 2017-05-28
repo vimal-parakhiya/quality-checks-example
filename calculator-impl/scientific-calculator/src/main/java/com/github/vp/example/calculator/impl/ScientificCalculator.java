03414bd1a747a40f3c1d30efde5a25e5387d1904
Vimal Parakhiya <vimalpar@INvimalpar.local>
2017-05-28 12:52:39 +0530

    Added sonarbug - array copy using for loop

--- calculator-impl/scientific-calculator/src/main/java/com/github/vp/example/calculator/impl/ScientificCalculator.java
+++ calculator-impl/scientific-calculator/src/main/java/com/github/vp/example/calculator/impl/ScientificCalculator.java
@@ -19,6 +19,11 @@ public class ScientificCalculator implements Calculator {
     }
 
     public double divide(double operand1, double operand2) {
+        int[] intArray = new int[10];
+        int[] anotherArray = new int[10];
+        for (int i=0; i<intArray.length; ++i) {
+            anotherArray[i] = intArray[i];
+        }
         return operand1 / operand2;
     }
 
