ee8782088045ab15aee0c5059cf3120b389e0b57
Vimal Parakhiya <vimalpar@INvimalpar.local>
2017-05-28 11:07:04 +0530

    Revert "Fixed sonar bugs"
    This reverts commit 556390cb081d09ad60ef17c332d1e93194314b5f.

---
+++ calculator-impl/scientific-calculator/src/main/java/com/github/vp/example/calculator/impl/ScientificCalculatorFaultyClass.java
@@ -0,0 +1,52 @@
+package com.github.vp.example.calculator.impl;
+
+/**
+ * Created by vimalpar on 07/05/17.
+ */
+public class ScientificCalculatorFaultyClass {
+    public static void FaultyFunctionInScientificCalculator() {
+        int x = 10;
+        int y=10;
+        if(x > y) {
+            if(x > y) {
+
+            }
+        }
+    }
+
+    public static void duplicateFunction1InScientificCalculator() {
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+    }
+
+    public static void duplicateFunction2InScientificCalculator() {
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+        System.out.println("Hello World in Duplicate Function...");
+    }
+
+    public static void fooInScientificCalculator(int[] input) {
+        input[10] = 10;
+    }
+}
