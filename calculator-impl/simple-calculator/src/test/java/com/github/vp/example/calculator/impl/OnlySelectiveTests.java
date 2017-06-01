234ceb078279525837c9f6bf59985c92dbf3dae2
Vimal Parakhiya <vimalpar@INvimalpar.local>
2017-06-01 21:44:09 +0530

    Added profile OnlySelectiveTests to demonstrate selective execution of junits

---
+++ calculator-impl/simple-calculator/src/test/java/com/github/vp/example/calculator/impl/OnlySelectiveTests.java
@@ -0,0 +1,7 @@
+package com.github.vp.example.calculator.impl;
+
+/**
+ * Created by vimalpar on 01/06/17.
+ */
+public interface OnlySelectiveTests {
+}
--- calculator-impl/simple-calculator/src/test/java/com/github/vp/example/calculator/impl/SimpleCalculatorTest.java
+++ calculator-impl/simple-calculator/src/test/java/com/github/vp/example/calculator/impl/SimpleCalculatorTest.java
@@ -1,16 +1,18 @@
 package com.github.vp.example.calculator.impl;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.data.Percentage.withPercentage;
-import static org.junit.Assert.*;
 
 /**
  * Created by vimalpar on 27/05/17.
  */
 public class SimpleCalculatorTest {
     private SimpleCalculator calculator = new SimpleCalculator();
+
+    @Category(OnlySelectiveTests.class)
     @Test
     public void add() throws Exception {
         assertThat(calculator.add(5.5, 4.4)).isEqualTo(9.9);
@@ -26,6 +28,7 @@ public class SimpleCalculatorTest {
         assertThat(calculator.multiply(5.4, 2)).isEqualTo(10.8);
     }
 
+    @Category(OnlySelectiveTests.class)
     @Test
     public void divide() throws Exception {
         assertThat(calculator.divide(5.4, 2)).isEqualTo(2.7);
